0184c618b4552f8f38ad8665332bfdcc7d867e0f
MrBisquit <69386111+MrBisquit@users.noreply.github.com>
2023-02-24 21:16:23 +0000

    Add files via upload

---
+++ Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("test");
+            list.Add("test@example.com");
+            Console.WriteLine(uniqueit.Identity.GenerateUUID(list, 5));
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(uniqueit.Identity.RandomString(i));
+            }
+        }
+    }
+}
