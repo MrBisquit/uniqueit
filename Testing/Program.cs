13932e72ca48a76835a52f297e8382a3e889292e
MrBisquit <william@wtdawson.info>
2023-02-25 11:54:31 +0000

    Added a GUID manipulator and some other things.

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -12,6 +12,21 @@
             {
                 Console.WriteLine(uniqueit.Identity.RandomString(i));
             }
+            Guid guid = uniqueit.Identity.GuidManipulator.GenerateGuid();
+            string guids = uniqueit.Identity.GuidManipulator.GenerateGuidToString();
+            byte[] guidbytes = uniqueit.Identity.GuidManipulator.GuidToBytes(guid);
+            Guid guidobject = uniqueit.Identity.GuidManipulator.BytesToGuid(guidbytes);
+            Console.WriteLine(guid.ToString());
+            Console.WriteLine(guids.ToString());
+            string a = "";
+            for (int i = 0; i < guidbytes.Length; i++)
+            {
+                a += guidbytes[i].ToString();
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(guidobject.ToString());
+            Console.WriteLine(uniqueit.Identity.RandomInt(0, 100, 5));
+            Console.WriteLine(uniqueit.Identity.RandomDouble(0, 100, 5));
         }
     }
 }
--- uniqueit/Identity.cs
+++ uniqueit/Identity.cs
@@ -57,5 +57,81 @@
 
             return output;
         }
+        public class GuidManipulator
+        {
+            /// <summary>
+            /// Creates a GUID object.
+            /// </summary>
+            /// <returns>A GUID object</returns>
+            public static Guid GenerateGuid()
+            {
+                return Guid.NewGuid();
+            }
+            /// <summary>
+            /// Creates a GUID object and puts it into a string.
+            /// </summary>
+            /// <returns>A GUID string</returns>
+            public static string GenerateGuidToString()
+            {
+                return Guid.NewGuid().ToString();
+            }
+            /// <summary>
+            /// Converts a GUID object to an array of bytes.
+            /// </summary>
+            /// <param name="guid">A GUID object</param>
+            /// <returns>An array of bytes that came from the GUID object</returns>
+            public static byte[] GuidToBytes(Guid guid)
+            {
+                return guid.ToByteArray();
+            }
+            /// <summary>
+            /// Converts bytes to a GUID
+            /// </summary>
+            /// <param name="bytes">An array of bytes</param>
+            /// <returns>A GUID object</returns>
+            public static Guid BytesToGuid(byte[] bytes)
+            {
+                return new Guid(bytes);
+            }
+        }
+        /// <summary>
+        /// Generates a random integer.
+        /// </summary>
+        /// <param name="minValue">The minimum value for the random integer</param>
+        /// <param name="maxValue">The maximum value for the random integer</param>
+        /// <param name="seed">The seed for the random class to use. (You can leave this blank)</param>
+        /// <returns>A randomly generated integer generated with the parameters you provided.</returns>
+        public static int RandomInt(int minValue, int maxValue, int? seed)
+        {
+            Random r;
+            if (seed.HasValue)
+            {
+                r = new Random(seed.Value);
+            }
+            else
+            {
+                r = new Random();
+            }
+            return r.Next(minValue, maxValue);
+        }
+        /// <summary>
+        /// Generates a random double.
+        /// </summary>
+        /// <param name="minValue">The minimum value for the random double</param>
+        /// <param name="maxValue">The maximum value for the random double</param>
+        /// <param name="seed">The seed for the random class to use. (You can leave this blank)</param>
+        /// <returns>A randomly generated double generated with the parameters you provided.</returns>
+        public static double RandomDouble(double minValue, double maxValue, int? seed)
+        {
+            Random r;
+            if(seed.HasValue)
+            {
+                r = new Random(seed.Value);
+            } else
+            {
+                r = new Random();
+            }
+            return r.NextDouble() * (maxValue - minValue) + minValue;
+        }
     }
 }
