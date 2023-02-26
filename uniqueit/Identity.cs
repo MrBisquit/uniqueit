2f331b0785a71d95ba120ef99ffcf93df4a7b562
MrBisquit <william@wtdawson.info>
2023-02-26 10:37:01 +0000

    Renamed "GenerateUUID" to "UniqueID"

--- uniqueit/Identity.cs
+++ uniqueit/Identity.cs
@@ -6,9 +6,9 @@
         /// <summary>
         /// Generates a string of 25 characters that use the characters in the seed as an input.
         /// </summary>
-        /// <param name="seed">A list of strings can include username, name, email and any others that can effect how the UUID is generated.</param>
-        /// <returns>A UUID generated based off of the data given.</returns>
-        public static string GenerateUUID(List<string> seed, int? id, int length = 25)
+        /// <param name="seed">A list of strings can include username, name, email and any others that can effect how the UniqueID is generated.</param>
+        /// <returns>A UniqueID generated based off of the data given.</returns>
+        public static string UniqueID(List<string> seed, int? id, int length = 25)
         {
             if (id == null) id = 0;
             List<char> chars = new List<char>();
